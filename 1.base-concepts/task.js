329917936947d8c71c97c92e6225b68773c7c2d0
Юля Азанова <yulyanovoselova92@gmail.com>
2025-06-23 20:07:53 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,13 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d == 0) {
+    arr = [-b/(2*a)];
+  } else if (d > 0) {
+    arr = [(-b + Math.sqrt(d) )/(2*a), (-b - Math.sqrt(d) )/(2*a)]; 
+  }
+
   return arr;
 }
 
