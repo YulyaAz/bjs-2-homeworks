c0dc441acd9918c435cc33cfaa8ec9d8f53289de
Юля Азанова <yulyanovoselova92@gmail.com>
2025-06-26 00:41:39 +0500

    Второе задание полностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,45 @@
 function getArrayParams(...arr) {
+	let min, max, avg, sum;
+	sum = 0;
+	min = 0;
+	max = 0;
+	if (arr.length == 1) {
+		min = arr[0];
+		max = arr[0];
+		sum = arr[0];
+	} else {
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i] > max) {
+				max = arr[i]
+			} else if (arr[i] < min) {
+				min = arr[i]
+			}
+			sum += arr[i];
+		}
 
-  return { min: min, max: max, avg: avg };
+	}
+
+	if (arr.length) {
+		avg = (sum / arr.length).toFixed(2);
+		avg = Number(avg);
+	}
+
+	return { min: min, max: max, avg: avg };
 }
+
+getArrayParams(-99, 99, 10)
+getArrayParams(1, 2, 3, -100, 10)
+getArrayParams(5)
+
+
+
+
+
+
+
+
+
+
 
 function summElementsWorker(...arr) {
 
