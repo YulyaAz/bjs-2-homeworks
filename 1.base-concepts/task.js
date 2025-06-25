7276308577b7dddcc4c5347b2478383673520146
Юля Азанова <yulyanovoselova92@gmail.com>
2025-06-25 22:31:46 +0500

    Второе задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -12,5 +12,15 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+let monthlyRate = (percent / 100) / 12;
+let loanBody = amount - contribution;
+if (loanBody <= 0) {
+return 0;
 }
+let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+let totalAmount = monthlyPayment * countMonths;
+return Math.round(totalAmount * 100) / 100;
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12)); 
+}
+
